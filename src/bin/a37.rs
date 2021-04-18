ef039c2b6c151a8d2c1deadea5495f1d4c2d42a3
Jayson Lennon <jayson@jaysonlennon.dev>
2021-04-17 22:49:26 -0700

    Fix tests

--- src/bin/a37.rs
+++ src/bin/a37.rs
@@ -78,46 +78,38 @@ mod test {
     fn converts_valid_hex_color() {
         let expected = Rgb(0, 204, 102);
         let actual = Rgb::try_from("#00cc66");
-        assert_eq!(
-            actual.is_ok(),
-            true,
-            "valid hex code should be converted to Rgb"
-        );
+        assert!(actual.is_ok(), "valid hex code should be converted to Rgb");
         assert_eq!(actual.unwrap(), expected, "wrong Rgb value");
     }
 
     #[test]
     fn fails_on_invalid_hex_digits() {
-        assert_eq!(
+        assert!(
             Rgb::try_from("#0011yy").is_err(),
-            true,
             "should be an error with invalid hex color"
         );
     }
 
     #[test]
     fn fails_when_missing_hash() {
-        assert_eq!(
+        assert!(
             Rgb::try_from("001100").is_err(),
-            true,
             "should be an error when missing hash symbol"
         );
     }
 
     #[test]
     fn fails_when_missing_color_components() {
-        assert_eq!(
+        assert!(
             Rgb::try_from("#0011f").is_err(),
-            true,
             "should be an error when missing one or more color components"
         );
     }
 
     #[test]
     fn fails_with_too_many_color_components() {
-        assert_eq!(
+        assert!(
             Rgb::try_from("#0011ffa").is_err(),
-            true,
             "should be an error when too many color components are provided"
         );
     }
