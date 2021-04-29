ac38a898ab0b68be391b686278e6d8dcdc4080b8
Jayson Lennon <jayson@jaysonlennon.dev>
2021-04-29 01:44:20 -0700

    Add a41 solution

---
+++ src/bin/a41.rs
@@ -0,0 +1,167 @@
+// Topic: Arc, Mutex, and Threads
+//
+// Summary:
+// * Create a multi-threaded program to simulate a copy machine.
+//
+//   A partial program has already been written as a guide. Implement
+//   the remaining functionality to complete the simulation.
+//
+// Requirements:
+// * The total number of pages must be displayed after each copy
+// * The user must be able to cancel a print job
+// * The copier has the following limitations:
+//   - The size of the print buffer only has enough capacity for 3 pages
+//   - The printer takes 500ms to print a page
+// * Use the println! macro to display messages for the simulation
+//
+// Notes:
+// * Ensure following crates are added to your Cargo.toml file:
+//   - crossbeam-channel
+//   - colored
+//   - parking_lot
+
+use colored::Colorize;
+use crossbeam_channel::{unbounded, Sender};
+use parking_lot::Mutex;
+use std::collections::VecDeque;
+use std::sync::Arc;
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
+
+enum CopierMsg {
+    Cancel,
+    Shutdown,
+}
+
+struct Copier {
+    buffer: Arc<Mutex<VecDeque<String>>>,
+    thread: JoinHandle<()>,
+    tx: Sender<CopierMsg>,
+}
+
+impl Copier {
+    fn print(buffer: Arc<Mutex<VecDeque<String>>>) -> bool {
+        let mut buffer = buffer.lock();
+        if let Some(page) = buffer.pop_front() {
+            println!("{} {}", "Print:".black().on_yellow(), page);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn cancel_jobs(buffer: Arc<Mutex<VecDeque<String>>>) {
+        let mut buffer = buffer.lock();
+        buffer.clear();
+        println!("{}", "Jobs canceled".black().on_red());
+    }
+
+    fn new() -> Self {
+        let (tx, rx) = unbounded();
+        let buffer = Arc::new(Mutex::new(VecDeque::new()));
+
+        let buffer_arc = Arc::clone(&buffer);
+        let thread = thread::spawn(move || {
+            let mut num_copies = 0;
+            loop {
+                if Self::print(Arc::clone(&buffer_arc)) {
+                    num_copies += 1;
+                    println!("Pages copied: {}", num_copies);
+                }
+
+                thread::sleep(Duration::from_millis(500));
+
+                if let Ok(msg) = rx.try_recv() {
+                    match msg {
+                        CopierMsg::Cancel => {
+                            Self::cancel_jobs(Arc::clone(&buffer_arc));
+                        }
+                        CopierMsg::Shutdown => {
+                            println!("{}", "Copier shutting down...".blue());
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        Self { buffer, thread, tx }
+    }
+
+    fn add_job(&self, page: &str) -> bool {
+        let mut buffer = self.buffer.lock();
+        if buffer.len() < 3 {
+            buffer.push_back(page.to_string());
+            true
+        } else {
+            false
+        }
+    }
+
+    fn cancel(&self) {
+        self.tx.send(CopierMsg::Cancel);
+    }
+
+    fn wait_until_done(&self) {
+        loop {
+            let buffer = self.buffer.lock();
+            if buffer.is_empty() {
+                break;
+            } else {
+                thread::sleep(Duration::from_millis(100));
+            }
+        }
+    }
+
+    fn shutdown(self) {
+        self.tx.send(CopierMsg::Shutdown);
+        self.thread.join();
+    }
+}
+
+enum Job<'a> {
+    Cancel,
+    Copy(&'a str),
+}
+
+fn send_jobs(copier: &Copier, mut jobs: VecDeque<Job>) {
+    loop {
+        if let Some(job) = jobs.pop_front() {
+            match job {
+                Job::Copy(page) => {
+                    if !copier.add_job(page) {
+                        println!("{}", "Buffer full".red());
+                        jobs.push_front(Job::Copy(page));
+                        thread::sleep(Duration::from_millis(100));
+                    } else {
+                        println!("{}", "Job added".cyan());
+                    }
+                }
+                Job::Cancel => {
+                    copier.cancel();
+                }
+            }
+        } else {
+            println!("{}", "All jobs added".bright_green());
+            break;
+        }
+    }
+}
+
+fn main() {
+    let mut jobs = VecDeque::new();
+    jobs.push_back(Job::Copy("page A"));
+    jobs.push_back(Job::Copy("page B"));
+    jobs.push_back(Job::Copy("page C"));
+    jobs.push_back(Job::Copy("page D"));
+    jobs.push_back(Job::Copy("page E"));
+    jobs.push_back(Job::Copy("page F"));
+    // jobs.push_back(Job::Cancel);   // uncomment this line to test job cancelation
+
+    let copier = Copier::new();
+
+    send_jobs(&copier, jobs);
+
+    copier.wait_until_done();
+    copier.shutdown();
+}
