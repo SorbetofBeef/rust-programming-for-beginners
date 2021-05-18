83cf5904911822ec1530cb7801b936ec02e5cefe
Jayson Lennon <jayson@jaysonlennon.dev>
2021-05-18 11:07:09 -0700

    .

--- src/bin/a41.rs
+++ src/bin/a41.rs
@@ -1,181 +1,156 @@
 // Topic: Arc, Mutex, and Threads
 //
 // Summary:
-// * Create a multi-threaded program to simulate a copy machine.
-//
-//   A partial program has already been written as a guide. Implement
-//   the remaining functionality on the Copier struct to complete the simulation.
+// Modify the existing multi-threaded program to include a global
+// counter shared among the threads. The counter should increase
+// by 1 whenever a worker completes a job.
 //
 // Requirements:
-// * The total number of pages must be displayed after each copy
-// * The user must be able to cancel the print jobs
-// * The copier has the following limitations:
-//   - The size of the print buffer only has enough capacity for 3 pages
-// * Use the println! macro to display messages for the simulation
+// * The total number of jobs completed must be displayed
+//   at the end of the program.
+// * Use Arc & Mutex to share the total count among threads.
 //
 // Notes:
 // * Ensure following crates are added to your Cargo.toml file:
 //   - crossbeam-channel
-//   - colored
 //   - parking_lot
 
-use colored::Colorize;
-use crossbeam_channel::{unbounded, Sender};
-use parking_lot::Mutex;
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::collections::VecDeque;
-use std::sync::Arc;
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-/// Message copier can receive through a channel
-enum CopierMsg {
-    Shutdown,
+/// Job given to workers.
+#[derive(Clone)]
+enum Job {
+    Print(String),
+    Sum(isize, isize),
 }
 
-/// Copy machine
-struct Copier {
-    buffer: Arc<Mutex<VecDeque<String>>>,
-    /// Use .join() on this to wait for the thread to terminate
-    thread: JoinHandle<()>,
-    /// Use this to send messages to the copier thread through a channel
-    tx: Sender<CopierMsg>,
+/// Message sent to workers.
+#[derive(Clone)]
+enum Message {
+    AddJob(Job),
+    Quit,
 }
 
-impl Copier {
-    // Make a copy of a page. The copy operation takes 500ms.
-    fn make_copy(page: &str) {
-        thread::sleep(Duration::from_millis(500));
-        println!("{} {}", "Print:".black().on_yellow(), page);
+struct Worker<M> {
+    tx: Sender<M>,
+    _rx: Receiver<M>,
+    handle: JoinHandle<()>,
+}
+
+impl Worker<Message> {
+    #[allow(unused_must_use)]
+    fn add_job(&self, job: Job) {
+        self.tx.send(Message::AddJob(job));
     }
-
-    /// Create a new copier
-    fn new() -> Self {
-        let (tx, rx) = unbounded();
-        let deque: VecDeque<String> = VecDeque::new();
-        let buffer = Arc::new(Mutex::new(deque));
-
-        let buffer_arc = Arc::clone(&buffer);
-        let thread = thread::spawn(move || {
-            let mut num_copies = 0;
-            let buffer = buffer_arc;
-            loop {
-                // Print out a page from the page buffer
-                let page = {
-                    let mut buffer = buffer.lock();
-                    buffer.pop_front()
-                };
-                if let Some(page) = page {
-                    Self::make_copy(page.as_str());
-                    num_copies += 1;
-                    // Display the total number of pages that have been printed
-                    println!("Pages copied: {}", num_copies);
-                }
-
-                // Process CopierMsg received on a channel
-                if let Ok(msg) = rx.try_recv() {
-                    match msg {
-                        CopierMsg::Shutdown => {
-                            println!("{}", "Copier shutting down...".blue());
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-
-        Self { buffer, thread, tx }
+    #[allow(unused_must_use)]
+    fn join(self) {
+        self.handle.join();
     }
-
-    /// Add a new job to the buffer
-    fn add_job(&self, page: &str) -> bool {
-        let mut buffer = self.buffer.lock();
-        // The size of the print buffer only has enough capacity for 3 pages
-        if buffer.len() < 3 {
-            buffer.push_back(page.to_string());
-            true
-        } else {
-            false
-        }
+    #[allow(unused_must_use)]
+    fn send_msg(&self, msg: Message) {
+        self.tx.send(msg);
     }
+}
 
-    /// Cancel jobs in the buffer
-    fn cancel_all(&self) {
-        let mut buffer = self.buffer.lock();
-        buffer.clear();
-        println!("{}", "Jobs canceled".black().on_red());
-    }
-
-    /// Waits until all pages in the buffer are copied
-    fn wait_until_done(&self) {
+/// Create a new worker to receive jobs.
+fn spawn_worker() -> Worker<Message> {
+    let (tx, rx) = unbounded();
+    // We clone the receiving end here so we have a copy to give to the
+    // thread. This allows us to save the `tx` and `rx` into the Worker struct.
+    let rx_thread = rx.clone();
+    // Spawn a new thread.
+    let handle = thread::spawn(move || {
+        // VecDeque allows us to get jobs in the order they arrive.
+        let mut jobs = VecDeque::new();
+        // Outer loop is so we can have a brief delay when no
+        // jobs are available.
         loop {
-            let is_empty = {
-                let buffer = self.buffer.lock();
-                buffer.is_empty()
-            };
-            if is_empty {
-                break;
-            } else {
-                thread::sleep(Duration::from_millis(100));
-            }
-        }
-    }
-
-    /// Turn off copier
-    fn shutdown(self) {
-        self.tx.send(CopierMsg::Shutdown);
-        self.thread.join();
-    }
-}
-
-/// Possible jobs the copier can handle
-enum Job<'a> {
-    CancelAll,
-    Copy(&'a str),
-}
-
-/// Helper function to ensure all jobs eventually get sent to the copier
-fn send_jobs(copier: &Copier, mut jobs: VecDeque<Job>) {
-    loop {
-        // Get next job
-        if let Some(job) = jobs.pop_front() {
-            match job {
-                Job::Copy(page) => {
-                    // Try to add the copy job
-                    if !copier.add_job(page) {
-                        println!("{}", "Buffer full".red());
-                        // Put the job back into the queue
-                        jobs.push_front(Job::Copy(page));
-                        thread::sleep(Duration::from_millis(100));
-                    } else {
-                        println!("{}", "Job added".cyan());
+            // Inner loop continuously processes jobs until
+            // no more are available.
+            loop {
+                // Get the next job.
+                for job in jobs.pop_front() {
+                    match job {
+                        Job::Print(msg) => println!("{}", msg),
+                        Job::Sum(lhs, rhs) => println!("{}+{}={}", lhs, rhs, lhs + rhs),
                     }
                 }
-                Job::CancelAll => {
-                    copier.cancel_all();
-                    return;
+                // Check for messages on the channel.
+                if let Ok(msg) = rx_thread.try_recv() {
+                    match msg {
+                        Message::AddJob(job) => {
+                            // When we receive a new job, add it
+                            // to the jobs list.
+                            jobs.push_back(job);
+                            // Continue processing jobs.
+                            continue;
+                        }
+                        Message::Quit => return,
+                    }
+                } else {
+                    // No messages on the channel, break from inner loop
+                    // and thread will wait momentarily for more messages.
+                    break;
                 }
             }
-        } else {
-            println!("{}", "All jobs added".bright_green());
-            break;
+            // Pause to wait for more messages to arrive on channel.
+            thread::sleep(Duration::from_millis(100));
         }
+    });
+
+    Worker {
+        tx,
+        _rx: rx,
+        handle,
     }
 }
 
 fn main() {
-    let mut jobs = VecDeque::new();
-    jobs.push_back(Job::Copy("page A"));
-    jobs.push_back(Job::Copy("page B"));
-    jobs.push_back(Job::Copy("page C"));
-    jobs.push_back(Job::Copy("page D"));
-    jobs.push_back(Job::Copy("page E"));
-    jobs.push_back(Job::Copy("page F"));
-    jobs.push_back(Job::CancelAll); // uncomment this line to test job cancelation
+    let jobs = vec![
+        Job::Print("hello".to_owned()),
+        Job::Sum(2, 2),
+        Job::Print("world".to_owned()),
+        Job::Sum(4, 4),
+        Job::Print("two words".to_owned()),
+        Job::Sum(1, 1),
+        Job::Print("a print job".to_owned()),
+        Job::Sum(10, 10),
+        Job::Print("message".to_owned()),
+        Job::Sum(3, 4),
+        Job::Print("thread".to_owned()),
+        Job::Sum(9, 8),
+        Job::Print("rust".to_owned()),
+        Job::Sum(1, 2),
+        Job::Print("compiler".to_owned()),
+        Job::Sum(9, 1),
+    ];
 
-    let copier = Copier::new();
+    let mut workers = vec![];
+    // Spawn 4 workers to process jobs.
+    for _ in 0..4 {
+        let worker = spawn_worker();
+        workers.push(worker);
+    }
 
-    send_jobs(&copier, jobs);
+    // Create an iterator that cycles through each worker endlessly.
+    let mut worker_ring = workers.iter().cycle();
+    for job in jobs.into_iter() {
+        // Get next worker
+        let worker = worker_ring.next().expect("failed to get worker");
+        // Add the job
+        worker.add_job(job);
+    }
 
-    copier.wait_until_done();
-    copier.shutdown();
+    // Ask all workers to quit.
+    for worker in &workers {
+        worker.send_msg(Message::Quit);
+    }
+
+    // Wait for workers to terminate.
+    for worker in workers {
+        worker.join();
+    }
 }
